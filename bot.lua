35ac703f05bd8abd50a3096c9f1e75930089a7fc
ahlanjuliusderamos-netizen <ahlan.julius.deramos@gmail.com>
2026-05-20 20:59:31 +0800

    Refactor HTTP server initialization in bot.lua

--- bot.lua
+++ bot.lua
@@ -1,10 +1,10 @@
-local http = require("http")
-local port = process.env.PORT or 10000
+local httpServer = require("http")
+local webPort = os.getenv("PORT") or 10000
 
-http.createServer(function(req, res)
+httpServer.createServer(function(req, res)
     res:setHeader("Content-Type", "text/plain")
     res:finish("Bot is awake and running!")
-end):listen(port)-- DRAWPS - Growsoft Discord Status Bot (Lua Version)
+end):listen(webPort)
 -- Uses the Discordia package
 
 local discordia = require('discordia')
